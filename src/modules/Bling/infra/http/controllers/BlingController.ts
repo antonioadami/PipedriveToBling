1dd3e638ede0fedb83272956430ccab82a4c6326
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-12 17:46:39 -0300

    Adicionando log

--- src/modules/Bling/infra/http/controllers/BlingController.ts
+++ src/modules/Bling/infra/http/controllers/BlingController.ts
@@ -3,15 +3,17 @@ import { container } from 'tsyringe';
 import SearchProductsService from '../../services/SearchProductsService';
 
 export default class BlingController {
-    public async GetProducts(resquest: Request, response: Response) {
+    public async GetProducts(request: Request, response: Response) {
         const searchProducts = container.resolve(SearchProductsService);
         const data = await searchProducts.execute();
         return response.status(200).json(data);
     }
 
-    public async NewSale(resquest: Request, response: Response) {
+    public async NewSale(request: Request, response: Response) {
         // const searchProducts = container.resolve(SearchProductsService);
         // const data = await searchProducts.execute();
+        console.log(request.body);
+
         return response.status(200).json();
     }
 }
