d22a742fa9840c7373c84a1c6e2c42be6ad73542
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-12 16:57:55 -0300

    Adding port to .env

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,6 @@ const app = new App();
 
 dotenv.config();
 
-app.server.listen(3333, () => {
+app.server.listen(process.env.PORT || 3333, () => {
     console.log('Server started on port 3333!');
 });
