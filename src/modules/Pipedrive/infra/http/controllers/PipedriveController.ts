ebeb3c353af8d73c6fcf3f1b7a86b1f27ea78ec3
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-13 13:24:15 -0300

    Enviar pedidos de acordo com deals

--- src/modules/Pipedrive/infra/http/controllers/PipedriveController.ts
+++ src/modules/Pipedrive/infra/http/controllers/PipedriveController.ts
@@ -1,3 +1,4 @@
+import SendOrderService from '@modules/Bling/infra/services/SendOrderService';
 import GetWonDealsService from '@modules/Pipedrive/services/GetWonDealsService';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
@@ -5,7 +6,17 @@ import { container } from 'tsyringe';
 export default class PipedriveController {
     public async GetWonDeals(request: Request, response: Response) {
         const getWonDeals = container.resolve(GetWonDealsService);
+        const sendOrder = container.resolve(SendOrderService);
         const data = await getWonDeals.execute();
-        return response.status(200).json(data);
+
+        if (data.success) {
+            const orderPromises = data.data.map(deal => {
+                return sendOrder.execute(deal.user_id.name);
+            });
+
+            Promise.all(orderPromises).then(order =>
+                response.status(200).json(order),
+            );
+        }
     }
 }
--- src/modules/Pipedrive/services/GetWonDealsService.ts
+++ src/modules/Pipedrive/services/GetWonDealsService.ts
@@ -1,6 +1,28 @@
 import IHttpRequestsProvider from '@shared/container/providers/httpRequests/models/IHttpRequestsProvider';
 import { inject, injectable } from 'tsyringe';
 
+interface Deal {
+    success: boolean;
+    data: Data[];
+}
+
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    has_pic: number;
+    pic_hash: string;
+    active_flag: true;
+    value: number;
+}
+
+interface Data {
+    id: number;
+    creator_user_id: User;
+    user_id: User;
+    person_id: number;
+}
+
 @injectable()
 export default class GetWonDealsService {
     constructor(
@@ -8,7 +30,7 @@ export default class GetWonDealsService {
         private httpRequestsProvider: IHttpRequestsProvider,
     ) {}
 
-    async execute(): Promise<void> {
+    async execute(): Promise<Deal> {
         const deals = await this.httpRequestsProvider.get(
             `${process.env.PIPEDRIVE_BASE_URL}/deals?status=won&start=0&api_token=${process.env.PIPEDRIVE_API_TOKEN}`,
         );
