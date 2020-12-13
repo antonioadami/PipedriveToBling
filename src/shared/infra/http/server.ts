80f1967c4e6bb77542178ef65fbb7d5e696bab94
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-13 19:58:56 -0300

    Correção dotenv

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import App from './app';
 
-const app = new App();
-
 dotenv.config();
+
+const app = new App();
 
 app.app.listen(process.env.PORT || 3333, () => {
     console.log('Server started on port 3333!');
--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-console.log(process.env.MONGO_URL);
-
 mongoose.connect(
     'mongodb+srv://antonio:antonio@cluster0.y80kx.mongodb.net/AntonioDB?retryWrites=true&w=majority',
     {
